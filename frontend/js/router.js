865658ead55bc1dd5a58b6fdd2ee7b0056f28054
Rhyan <rfookune@macalester.edu>
2015-02-20 23:22:10 -0600

    initial folder frontend

--- frontend/js/router.js
+++ frontend/js/router.js
@@ -1,18 +1,24 @@
-define(['jquery','underscore','backbone','views/eventListView'],
-	function($,_,Backbone,eventListView){
+define(['jquery','underscore','backbone','views/searchListView'],
+	function($,_,Backbone, searchListView){
 		var AppRouter = Backbone.Router.extend({
 			routes: {
-				"*actions": "defaultAction"
+				"/": "home",
+				// "*actions": "defaultAction"
+				"search" : "searchList"
 			}
 
 		});
 
 		var initialize = function() {
 			var app_router = new AppRouter();
-			app_router.on("route:defaultAction", function(actions){
-				var dashboard = new eventListView();
+			// app_router.on("route:defaultAction", function(actions){
+			// 	var dashboard = new eventListView();
+			// 	dashboard.render();
+			// });
+			app_router.on('route:searchList', function(){
+				var dashboard = new searchListView();
 				dashboard.render();
-			});
+			})
 
 			Backbone.history.start();
 		}
---
+++ frontend/js/views/searchListView.js
@@ -0,0 +1,14 @@
+define(['jquery','underscore','backbone','bootstrap','text!templates/search-list.html','views/userProfileView'],
+	function($,_,Backbone,bootstrap,searchListTemplate,userProfileView){
+		var searchListView = Backbone.View.extend({
+		el: $("#dashboard"),
+		initialize: function(){
+			this.$el.html(_.template(searchListTemplate));
+		},
+		render: function(){
+		}
+
+		});
+		return searchListView;
+
+	});

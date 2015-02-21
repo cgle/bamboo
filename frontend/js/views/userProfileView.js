5a15296b21b2854db37a56c0758db1cd9b2a5579
cgle <cle@macalester.edu>
2015-02-21 10:16:13 -0600

    update

--- frontend/js/views/userProfileView.js
+++ frontend/js/views/userProfileView.js
@@ -1,14 +1,22 @@
-define(['jquery','underscore','backbone','bootstrap','text!templates/user-profile.html','views/searchListView'],
-	function($,_,Backbone,bootstrap,userProfileTemplate,searchListView){
+define(['jquery','underscore','backbone','bootstrap','text!templates/user-profile.html'],
+	function($,_,Backbone,bootstrap,userProfileTemplate){
 		var userProfileView = Backbone.View.extend({
 		el: $("#dashboard"),
 		initialize: function(){
 			this.$el.html(_.template(userProfileTemplate));
+			this.play();
 		},
 		render: function(){
+
+		},
+		play: function() {
+			var socket = io();
+			socket.on('broadcasting', function(stream) {
+
+			});
 		}
 
-		});
-		
+	});
+
 	return userProfileView;
 });
